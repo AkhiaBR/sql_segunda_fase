0cb8eb7c68d06245798f019311bd4ebd0eb6451f
Fernando <fernandooogoncalves@gmail.com>
2024-08-23 14:58:02 -0300

    commit_4

--- introducao_comandos_dml/funcao_select.sql
+++ introducao_comandos_dml/funcao_select.sql
@@ -1,1 +1,2 @@
 SELECT matricula,nome FROM 2190escola.aluno -- seleciona matricula e nome da table aluno dentro do database 2190escola
+WHERE matricula IS NULL; -- mostra somente os valores nulos dos campos matricula e nome
