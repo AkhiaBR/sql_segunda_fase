243f4151df1109b4f8a6095c4cf120cb0872d971
Fernando <65679737+AkhiaBR@users.noreply.github.com>
2024-08-23 13:58:01 -0300

    Add files via upload

---
+++ introducao_comandos_dml/update_chaves.sql
@@ -0,0 +1,3 @@
+SELECT * FROM 2190escola.estado; -- mostra todas as informacoes da tabela estado dentro do databse 2190escola
+update estado set nome = "Rio Grande do Sul" -- atualiza o estado setando o nome como Rio Grande do Sul
+where codigo_estado = 3 -- condicao de qual coluna sera atualizada
