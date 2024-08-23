fb936b949b5559877678e88bf213abcbbb3fc9da
Fernando <fernandooogoncalves@gmail.com>
2024-08-23 14:08:48 -0300

    commit_2

--- introducao_comandos_dml/update_chaves.sql
+++ introducao_comandos_dml/update_chaves.sql
@@ -1,3 +1,7 @@
 SELECT * FROM 2190escola.estado; -- mostra todas as informacoes da tabela estado dentro do databse 2190escola
 update estado set nome = "Rio Grande do Sul" -- atualiza o estado setando o nome como Rio Grande do Sul
-where codigo_estado = 3 -- condicao de qual coluna sera atualizada
+where codigo_estado = 3; -- condicao de qual coluna sera atualizada
+
+SELECT * FROM 2190escola.aluno;
+UPDATE aluno SET endereco = "Rua Pedro Loos" 
+WHERE matricula in (1,2); -- altera as colunas 1 e 2 para o mesmo endereco
