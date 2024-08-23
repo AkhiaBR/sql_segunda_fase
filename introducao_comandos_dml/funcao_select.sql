a213d09f856784a663979cd5edb060fb8953e9cf
Fernando <fernandooogoncalves@gmail.com>
2024-08-23 15:25:27 -0300

    commit_7

--- introducao_comandos_dml/funcao_select.sql
+++ introducao_comandos_dml/funcao_select.sql
@@ -3,3 +3,6 @@ WHERE matricula IS NULL; -- mostra somente os valores nulos dos campos matricula
 
 SELECT * FROM 2190escola.aluno
 WHERE endereco LIKE "R%"; -- mostra todos os alunos com enderecos com que comecam com "R"
+
+SELECT * FROM 2190escola.aluno
+WHERE matricula BETWEEN 2 and 3; -- mostra todos os alunos com a as matriculas de 2 e 3
