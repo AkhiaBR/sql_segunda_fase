0db34e060981a0c0c968eb1dc40b74892d789ec3
Fernando <fernandooogoncalves@gmail.com>
2024-08-23 15:05:39 -0300

    commit_6

--- introducao_comandos_dml/funcao_select.sql
+++ introducao_comandos_dml/funcao_select.sql
@@ -1,2 +1,5 @@
 SELECT matricula,nome FROM 2190escola.aluno -- seleciona matricula e nome da table aluno dentro do database 2190escola
 WHERE matricula IS NULL; -- mostra somente os valores nulos dos campos matricula e nome
+
+SELECT * FROM 2190escola.aluno
+WHERE endereco LIKE "R%"; -- mostra todos os alunos com enderecos com que comecam com "R"
