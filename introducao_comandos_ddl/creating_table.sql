9d130e4b31a0b5863170ec29ca5e204bd48123fa
Fernando <fernandooogoncalves@gmail.com>
2024-09-06 15:24:38 -0300

    first commit

---
+++ introducao_comandos_ddl/creating_table.sql
@@ -0,0 +1,12 @@
+CREATE TABLE matricula (
+codigo INT(5) NOT NULL,
+data_matricula DATE NOT NULL,
+hora TIME NOT NULL,
+codigo_aluno INT(5) NOT NULL,
+codigo_curso INT(5) NOT NULL,
+desconto FLOAT(8,2) NOT NULL,
+valor FLOAT(8,2) NOT NULL,
+Primary key (codigo),
+Foreign key (codigo_aluno) REFERENCES aluno(matricula),
+Foreign key (codigo_curso) REFERENCES curso(codigo_curso)
+);
