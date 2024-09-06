398e1d458c932b7071a93f05bac09b65255ef5b6
Fernando <fernandooogoncalves@gmail.com>
2024-09-06 15:13:08 -0300

    add: sum_avg.sql

--- introducao_comandos_dml/sum_avg.sql
+++ introducao_comandos_dml/sum_avg.sql
@@ -3,3 +3,6 @@ FROM 2190escola.disciplina;
 
 SELECT AVG(nraulas) -- average, média do número de aulas da table disciplina
 FROM 2190escola.disciplina;
+
+SELECT COUNT(nraulas) -- conta a quantidade de registros de um campo, por exemplo, 5 alunos tem 5 nraulas, ou seja, COUNT(nraulas) = 5
+FROM 2190escola.disciplina;
