35021002df544da8fd2e921c6ca5985a673228b4
Fernando <fernandooogoncalves@gmail.com>
2024-09-06 15:10:06 -0300

    add: sum_avg.sql

---
+++ introducao_comandos_dml/sum_avg.sql
@@ -0,0 +1,5 @@
+SELECT SUM(nraulas) -- total de aulas do campo nraulas da table disciplina
+FROM 2190escola.disciplina;
+
+SELECT AVG(nraulas) -- average, média do número de aulas da table disciplina
+FROM 2190escola.disciplina;
